ada74152877fe017c6573d70e22e3cee656b51e1
urvesh <urveshbogun@gmail.com>
2025-03-10 23:16:41 +0000

    Added CatCoin

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,8 +2,9 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import optionpriceicon from "../../Assets/Projects/resized_optionpriceicon.png";
+import optionspricingicon from "../../Assets/Projects/OptionsPricingIcon.png";
 import azurelogo from "../../Assets/Projects/resized_azurelogo.png";
+import catcoinicon from "../../Assets/Projects/CatCoinIcon.png";
 
 function Projects() {
   return (
@@ -19,23 +20,34 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={optionpriceicon}
-              isBlog={false}
+              imgPath={optionspricingicon}
               title="Computational Models to Calculate Option Prices"
               description="Implemented Black Scholes, Trees and Monte Carlo to calculate option prices for American or European Calls and puts from given parameters"
-              ghLink="https://github.com/Urvesh-Bogun/financial-computations"
-              demoLink="https://chatify-49.web.app/"
+              ghLink="https://github.com/Urvesh-Bogun/FinancialComputationsForOptionPrices"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={azurelogo}
-              isBlog={false}
               title="Deploying a SIEM"
-              description=" Created a Virtual Machine (VM) for SIEM monitoring and alerting. Configured Azure Log Analytics Workspace to collect logs from the VM. Implemented Microsoft Sentinel for monitoring and detection to threats. Set up custom alert rules in Sentinel to notify me on VM logins."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              description={
+                <ul style={{ textAlign: "left", paddingLeft: "20px" }}>
+                  <li>Created a Virtual Machine (VM) for SIEM monitoring and alerting.</li>
+                  <li>Configured Azure Log Analytics Workspace to collect logs from the VM.</li>
+                  <li>Implemented Microsoft Sentinel for monitoring and detection of threats.</li>
+                  <li>Set up custom alert rules in Sentinel to notify me on VM logins.</li>
+                </ul>
+              }
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={catcoinicon}
+              title="CatCoin Blockchain"
+              description="A simple Python implementation of a blockchain-like structure for managing transactions in a hypothetical cryptocurrency called CatCoin (CC)."
+              ghLink="https://github.com/Urvesh-Bogun/CatCoinBlockchain"
             />
           </Col>
         </Row>
